a93b4b01ab0145e7dccabdd4059e8cd75c6f8419
Sarah R <sarah.rakhamimov@baruchmail.cuny.edu>
2025-05-30 23:16:14 -0400

    update

--- gettingStarted.py
+++ gettingStarted.py
@@ -33,8 +33,10 @@ def welcome_assignment_answers(question):
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue and catch errors
-        answer = "This is an unexpected question."
+        raise Exception ("This is an unexpected question.")
+
     return(answer)
+
 # Complete all the questions.
 
 
