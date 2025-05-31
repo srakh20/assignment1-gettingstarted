0ed6cc495a5a6838da2469cce87eac5f35508fad
Sarah R <sarah.rakhamimov@baruchmail.cuny.edu>
2025-05-30 23:03:22 -0400

    new code

--- gettingStarted.py
+++ gettingStarted.py
@@ -1,26 +1,47 @@
 ### welcome_assignment_answers
 ### Input - All nine questions given in the assignment.
 ### Output - The right answer for the specific question.
+import hashlib
 
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
+
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap !"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        #answer = "18e9a3450d5abd41a74c401b1c5b7a705043434897e12562d10d020ff01a9a54"
+        input_string = "sr5649@nyu.edu"
+        hash_object = hashlib.sha256(input_string.encode('utf-8'))
+        answer = hash_object.hexdigest()
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 4
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+
+        
+        #"Are encoding and encryption the same? - Yes/No":
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue and catch errors
-        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+        answer = "This is an unexpected question."
     return(answer)
 # Complete all the questions.
 
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
+    #debug_question = "Are encoding and encryption the same? - Yes/No"
+    debug_question = "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"
     print(welcome_assignment_answers(debug_question))
 
 #Questions:
